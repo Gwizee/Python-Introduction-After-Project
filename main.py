ec8e16e6c2cf0802f332713eae236604ee9488a9
Gwizee <risingfire69@gmail.com>
2024-09-27 16:07:29 +0600

    Python Intraduction After Project is completed

---
+++ main.py
@@ -0,0 +1,6 @@
+numb1=234
+numb2=432
+print("The Addition of 234 and 432 is =",234+432)
+print("The Subtraction of 234 and 432 is =",234-432)
+print("The Multipication of 234 and 432 is =",234*432)
+print("The Division of 234 and 432 is =",234/432)
